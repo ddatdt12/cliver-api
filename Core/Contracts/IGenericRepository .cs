9b4f1f8a14e5e73ea9b4a1377917692fe27a9aa0
Đỗ Đạt <ddatdt12@gmail.com>
2022-07-16 23:10:45 +0700

    fix product database connection string

--- Core/Contracts/IGenericRepository .cs
+++ Core/Contracts/IGenericRepository .cs
@@ -2,14 +2,14 @@
 
 namespace CliverApi.Core.Contracts
 {
-    public interface IGenericRepository<T> where T : class
-    {
-        Task<IEnumerable<T>> GetAll ();
-        Task<T> FindById<TId>(TId id);
-        IQueryable<T> Find(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, string includeProperties = "");
-        Task<bool> Add(T entity);
-        Task<bool> Upsert(T entity);
-        Task<bool> Delete<TId>(TId id);
-        void Delete(T entityToDelete);
-    }
+  public interface IGenericRepository<T> where T : class
+  {
+    Task<IEnumerable<T>> GetAll();
+    Task<T> FindById<TId>(TId id);
+    IQueryable<T> Find(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, string includeProperties = "");
+    Task<bool> Add(T entity);
+    Task<bool> Upsert(T entity);
+    Task<bool> Delete<TId>(TId id);
+    void Delete(T entityToDelete);
+  }
 }
